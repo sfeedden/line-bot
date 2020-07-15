8db1ad7b7874eaeccb5b6597d60c0fa5550c4327
Steven Liang <gkg.yji@gmail.com>
2020-07-16 00:27:45 +0800

    Update

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ def handle_message(event):
             sticker_id='52114147'
         )
         line_bot_api.reply_message(
-        event.reply_token,
-        sticker_message)
+            event.reply_token,
+            sticker_message)
         return
     
     if msg == 'hi':
